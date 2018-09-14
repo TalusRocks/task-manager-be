85e7504646998320647146ed2ca601346c368ac3
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-09-14 16:32:44 -0700

    add dynamo to GET tasks

--- handlers/get-tasks.js
+++ handlers/get-tasks.js
@@ -1,20 +1,23 @@
-const tasks = require('../data/tasks.json')
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 function getTasks(taskId){
-  if (!taskId) {
-    return tasks
+  if (typeof taskId === 'undefined') {
+    return docClient.scan({
+      TableName: 'tasks'
+    }).promise()
+      .then(result => result.Items)
   }
 
-  const task = tasks.find((el) => {
-    return el.id == taskId
-  })
-
-  if (task) {
-    return task
-  }
+  return docClient.get({
+    TableName: 'tasks',
+    Key: {
+      taskId: taskId
+    }
+  }).promise()
+    .then(result => result.Item)
 
   throw new Error('The task you requested was not found')
-
 }
 
 module.exports = getTasks
