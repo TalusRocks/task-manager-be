a431f44ccadaf3b4bd7cfdc5e184735f4febc949
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-09-17 11:49:18 -0700

    add PUT and DELETE to Dynamo, remove local temp data

--- handlers/delete-task.js
+++ handlers/delete-task.js
@@ -1,11 +1,25 @@
-const tasks = require('../data/tasks')
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 function deleteTask(taskId){
   if (!taskId) {
     throw new Error('Could not find the task to delete')
   }
 
-  return {}
+  return docClient.delete({
+    TableName: 'tasks',
+    Key: {
+      taskId: taskId
+    }
+  }).promise()
+    .then((res) => {
+      console.log('Task deleted!', res)
+      return res
+    })
+    .catch((error) => {
+      console.log('Task not deleted', error);
+      throw error
+    })
 }
 
 module.exports = deleteTask
--- handlers/edit-task.js
+++ handlers/edit-task.js
@@ -1,14 +1,27 @@
-const tasks = require('../data/tasks.json')
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 function editTask(taskId, updatedTask){
   if (!taskId || !updatedTask) {
     throw new Error('Insufficient infromation to update task')
   }
 
-  return {
-    message: `Order ${taskId} was updated`
-  }
-  
+  return docClient.put({
+    TableName: 'tasks',
+    Item: {
+      taskId: taskId,
+      title: updatedTask.title
+    }
+  }).promise()
+    .then((res) => {
+      console.log('Task updated!', res)
+      return res
+    })
+    .catch((error) => {
+      console.log('Task not updated', error);
+      throw error
+    })
+
 }
 
 module.exports = editTask
